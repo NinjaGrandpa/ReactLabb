a76c882e89e2c1b635056bdfbd237e5853d6f334
NinjaGrandpa <max@jenslov.se>
2024-01-02 17:19:36 +0100

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5100,
     proxy: {
       "/scrap": {
         target: "http://localhost:5046",
